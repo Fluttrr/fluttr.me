5536fb20d39b830391f145d4153e2ed2258eb99f
Fluttrr <ySZ39@proton.me>
2025-06-24 14:57:01 +0200

    added latest blog post date and other minor changes

--- js/fetchJSON.js
+++ js/fetchJSON.js
@@ -4,6 +4,7 @@ fetch('./js/posts.json')
   .then(response => response.json())
   .then(data => {
     blogPosts = data.posts; // Store the posts in the global variable
+    document.getElementById("latest-blog-post").textContent = blogPosts[0].date;
   })
   .catch(error => console.error('Error fetching blog posts:', error));
 
