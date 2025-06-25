bb4ae9c137cfcc5669084c9bc2559054382a1bc2
Fluttrr <fluttr@proton.me>
2025-06-25 20:36:36 +0200

    pls forget that i forgot to add this case.. i swear im not stupid..

--- js/fetchJSON.js
+++ js/fetchJSON.js
@@ -69,6 +69,8 @@ function prettifyDate(date) {
 		daySuffix = "st"
 	else if (day == 2 || day == 22)
 		daySuffix = "nd"
+	else if (day == 3 || day == 23)
+		daySuffix = "rd"
 	else
 		daySuffix = "th"
 
